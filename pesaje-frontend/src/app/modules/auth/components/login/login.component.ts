896649c67b4fa68dcef743f6c111ea89192adc54
Jonathan Pillajo <pandc19@outlook.com>
2025-08-02 18:55:12 -0500

    Remove console log statement from login component to clean up code and improve maintainability.

--- pesaje-frontend/src/app/modules/auth/components/login/login.component.ts
+++ pesaje-frontend/src/app/modules/auth/components/login/login.component.ts
@@ -38,7 +38,6 @@ export class LoginComponent implements OnInit, OnDestroy {
     // get return url from route parameters or default to '/'
     this.returnUrl =
       this.route.snapshot.queryParams['returnUrl'.toString()] || '/';
-      console.log(this.returnUrl);
   }
 
   // convenience getter for easy access to form fields
