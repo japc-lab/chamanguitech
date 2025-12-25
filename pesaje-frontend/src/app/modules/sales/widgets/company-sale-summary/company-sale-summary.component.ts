68974bd2158f69dcdcb8aa98859be8287a508f47
Jonathan Pillajo <pandc19@outlook.com>
2025-12-25 10:14:36 -0500

    Remove console log for purchaseModel in CompanySaleSummaryComponent to clean up debugging output.

--- pesaje-frontend/src/app/modules/sales/widgets/company-sale-summary/company-sale-summary.component.ts
+++ pesaje-frontend/src/app/modules/sales/widgets/company-sale-summary/company-sale-summary.component.ts
@@ -61,7 +61,6 @@ export class CompanySaleSummaryComponent implements OnChanges {
     this.grandTotal = wholeSubtotal + tailSubtotal;
 
     // Calculate average purchase price
-    console.log('purchaseModel', this.purchaseModel);
     const poundsBought = this.purchaseModel?.totalPounds || 0;
     if (poundsBought > 0) {
       this.averagePurchasePrice =
